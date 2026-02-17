f9d576b045872c491dd9a856aa13b2c08896184a
Lucas Santos Magro <lucassmagro@gmail.com>
2026-02-17 15:28:33 -0300

    aula 13 e 14

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1;
+var b = 1;
+
+// Estrutura de repetição com teste lógico no início
+while (c <= 6) {
+  console.log(`Passo ${c}`);
+  c++; // c = c + 1
+}
+
+// Estrutura de repetição com teste lógico no fim
+do {
+  console.log(`Passo ${b}`);
+  b++; // c = c + 1
+} while (b <= 6);
+
+/*
+console.log("Passo 1");
+console.log("Passo 2");
+console.log("Passo 3");
+console.log("Passo 4");
+console.log("Passo 5");
+console.log("Passo 6");
+*/
